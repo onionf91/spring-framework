33aa9a40cb4df3996195fe633c904ae86950167e
Jason Yu <jason.yu@fuhu.com>
2017-08-10 10:01:06 +0800

    Define groovy markup config interface

--- spring-webflux/src/main/java/org/springframework/web/reactive/result/view/groovy/GroovyMarkupConfig.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/result/view/groovy/GroovyMarkupConfig.java
@@ -16,10 +16,23 @@
 
 package org.springframework.web.reactive.result.view.groovy;
 
+import groovy.text.markup.MarkupTemplateEngine;
+
 /**
+ * Interface to be implemented by objects that configure and manage a Groovy
+ * {@link MarkupTemplateEngine} for automatic lookup in a web environment.
+ * Detected and used by {@link GroovyMarkupView}.
  *
  * @author Jason Yu
  * @since 5.0
  */
 public interface GroovyMarkupConfig {
+
+	/**
+	 * Return the Groovy {@link MarkupTemplateEngine} for the current
+	 * web application context. May be unique to one servlet, or shared
+	 * in the root context.
+	 * @return the Groovy MarkupTemplateEngine engine
+	 */
+	MarkupTemplateEngine getTemplateEngine();
 }

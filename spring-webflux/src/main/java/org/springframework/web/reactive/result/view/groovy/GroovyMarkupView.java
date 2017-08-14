8cf8efdfb1fb8f6712d8e45ebd0deff588bb72f1
Jason Yu <jason.yu@fuhu.com>
2017-08-14 16:51:25 +0800

    Implement groovy markup view class

--- spring-webflux/src/main/java/org/springframework/web/reactive/result/view/groovy/GroovyMarkupView.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/result/view/groovy/GroovyMarkupView.java
@@ -16,10 +16,103 @@
 
 package org.springframework.web.reactive.result.view.groovy;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+
+import groovy.text.Template;
+import groovy.text.markup.MarkupTemplateEngine;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContextException;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.MediaType;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+import org.springframework.util.MimeType;
+import org.springframework.web.reactive.result.view.AbstractUrlBasedView;
+import org.springframework.web.server.ServerWebExchange;
+
 /**
  *
  * @author Jason Yu
  * @since 5.0
  */
-public class GroovyMarkupView {
+public class GroovyMarkupView extends AbstractUrlBasedView {
+
+	@Nullable
+	MarkupTemplateEngine engine;
+
+	public void setTemplateEngine(MarkupTemplateEngine templateEngine) {
+		this.engine = templateEngine;
+	}
+
+	protected MarkupTemplateEngine autodetectMarkupTemplateEngine() throws BeansException {
+		try {
+			return BeanFactoryUtils
+					.beanOfTypeIncludingAncestors(
+							obtainApplicationContext(),
+							GroovyMarkupConfig.class, true, false)
+					.getTemplateEngine();
+		} catch (NoSuchBeanDefinitionException e) {
+			throw new ApplicationContextException("Expected a single GroovyMarkupConfig bean in the current " +
+					"Servlet web application context or the parent root context: GroovyMarkupConfigurer is " +
+					"the usual implementation. This bean may have any name.", e);
+		}
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		super.afterPropertiesSet();
+		if (this.engine == null) {
+			setTemplateEngine(autodetectMarkupTemplateEngine());
+		}
+	}
+
+	@Override
+	public boolean checkResourceExists(Locale locale) throws Exception {
+		Assert.state(this.engine != null, "No MarkupTemplateEngine set");
+		try {
+			this.engine.resolveTemplate(getUrl());
+		} catch (IOException e) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	protected Mono<Void> renderInternal(
+			Map<String, Object> renderAttributes,
+			@Nullable MediaType contentType,
+			ServerWebExchange exchange) {
+
+		String url = getUrl();
+		Assert.state(url != null, "'url' not set");
+		Assert.state(this.engine != null, "No MarkupTemplateEngine set");
+
+		DataBuffer dataBuffer = exchange.getResponse().bufferFactory().allocateBuffer();
+		try {
+			Template template = this.engine.createTemplateByPath(url);
+			Charset charset = getCharset(contentType);
+			Writer writer = new OutputStreamWriter(dataBuffer.asOutputStream(), charset);
+			template.make(renderAttributes).writeTo(new BufferedWriter(writer));
+		} catch (IOException e) {
+		} catch (ClassNotFoundException e) {
+		}
+		return exchange.getResponse().writeWith(Flux.just(dataBuffer));
+	}
+
+	private Charset getCharset(@Nullable MediaType mediaType) {
+		return Optional.ofNullable(mediaType).map(MimeType::getCharset).orElse(getDefaultCharset());
+	}
+
 }

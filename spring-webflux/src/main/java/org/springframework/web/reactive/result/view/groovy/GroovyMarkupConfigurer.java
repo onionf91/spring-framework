2d46e04d2d8d11a4a8107e706a3c6ddfc9892b0c
Jason Yu <jason.yu@fuhu.com>
2017-08-14 11:31:11 +0800

    Implement groovy markup configurer class

--- spring-webflux/src/main/java/org/springframework/web/reactive/result/view/groovy/GroovyMarkupConfigurer.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/result/view/groovy/GroovyMarkupConfigurer.java
@@ -16,10 +16,134 @@
 
 package org.springframework.web.reactive.result.view.groovy;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import groovy.text.markup.MarkupTemplateEngine;
+import groovy.text.markup.TemplateConfiguration;
+import groovy.text.markup.TemplateResolver;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.core.io.Resource;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
 /**
  *
  * @author Jason Yu
  * @since 5.0
  */
-public class GroovyMarkupConfigurer {
+public class GroovyMarkupConfigurer extends TemplateConfiguration
+		implements GroovyMarkupConfig, ApplicationContextAware, InitializingBean {
+
+	private String resourceLoaderPath = "classpath:";
+
+	@Nullable
+	private MarkupTemplateEngine templateEngine;
+
+	@Nullable
+	private ApplicationContext applicationContext;
+
+	public void setResourceLoaderPath(String resourceLoaderPath) {
+		this.resourceLoaderPath = resourceLoaderPath;
+	}
+
+	public java.lang.String getResourceLoaderPath() {
+		return resourceLoaderPath;
+	}
+
+	public void setTemplateEngine(MarkupTemplateEngine templateEngine) {
+		this.templateEngine = templateEngine;
+	}
+
+	@Override
+	public MarkupTemplateEngine getTemplateEngine() {
+		Assert.state(this.templateEngine != null, "No MarkupTemplateEngine set");
+		return this.templateEngine;
+	}
+
+	protected ApplicationContext getApplicationContext() {
+		Assert.state(this.applicationContext != null, "No ApplicationContext set");
+		return this.applicationContext;
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if (this.templateEngine == null) {
+			this.templateEngine = createTemplateEngine();
+		}
+	}
+
+	protected MarkupTemplateEngine createTemplateEngine() throws IOException {
+		if (this.templateEngine == null) {
+			ClassLoader templateClassLoader = createTemplateClassLoader();
+			this.templateEngine = new MarkupTemplateEngine(templateClassLoader, this, new LocaleTemplateResolver());
+		}
+		return this.templateEngine;
+	}
+
+	protected ClassLoader createTemplateClassLoader() throws IOException {
+		String[] paths = StringUtils.commaDelimitedListToStringArray(getResourceLoaderPath());
+		List<URL> urls = new ArrayList<>();
+		for (String path: paths) {
+			Resource[] resources = getApplicationContext().getResources(path);
+			if (resources.length > 0) {
+				for (Resource resource : resources) {
+					if (resource.exists()) {
+						urls.add(resource.getURL());
+					}
+				}
+			}
+		}
+		ClassLoader classLoader = getApplicationContext().getClassLoader();
+		Assert.state(classLoader != null, "No ClassLoader");
+		return (urls.size() > 0 ? new URLClassLoader(urls.toArray(new URL[urls.size()]), classLoader) : classLoader);
+	}
+
+	protected URL resolveTemplate(ClassLoader classLoader, String templatePath) throws IOException {
+		MarkupTemplateEngine.TemplateResource resource = MarkupTemplateEngine.TemplateResource.parse(templatePath);
+		Locale locale = LocaleContextHolder.getLocale();
+		URL url = classLoader.getResource(resource.withLocale(locale.toString().replace("-", "_")).toString());
+		if (url == null) {
+			url = classLoader.getResource(resource.withLocale(locale.getLanguage()).toString());
+		}
+		if (url == null) {
+			url = classLoader.getResource(resource.withLocale(null).toString());
+		}
+		if (url == null) {
+			throw new IOException("Unable to load template:" + templatePath);
+		}
+		return url;
+	}
+
+	private class LocaleTemplateResolver implements TemplateResolver {
+
+		@Nullable
+		private ClassLoader classLoader;
+
+		@Override
+		public void configure(ClassLoader templateClassLoader, TemplateConfiguration configuration) {
+			this.classLoader = templateClassLoader;
+		}
+
+		@Override
+		public URL resolveTemplate(String templatePath) throws IOException {
+			Assert.state(this.classLoader != null, "No template Classloader available");
+			return GroovyMarkupConfigurer.this.resolveTemplate(this.classLoader, templatePath);
+		}
+	}
 }

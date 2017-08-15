aa55e7f9a9646084b89345b5fa0a809b6383dc99
Jason Yu <jason.yu@fuhu.com>
2017-08-15 09:54:39 +0800

    Implement groovy markup view test class

--- spring-webflux/src/test/java/org/springframework/web/reactive/result/view/groovy/GroovyMarkupViewTests.java
+++ spring-webflux/src/test/java/org/springframework/web/reactive/result/view/groovy/GroovyMarkupViewTests.java
@@ -16,8 +16,84 @@
 
 package org.springframework.web.reactive.result.view.groovy;
 
+import java.io.Reader;
+import groovy.text.Template;
+import groovy.text.TemplateEngine;
+import groovy.text.markup.MarkupTemplateEngine;
+import groovy.text.markup.TemplateConfiguration;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import org.springframework.beans.DirectFieldAccessor;
+import org.springframework.context.ApplicationContextException;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.mock.http.server.reactive.test.MockServerHttpRequest;
+import org.springframework.mock.http.server.reactive.test.MockServerWebExchange;
+
+import static org.junit.Assert.*;
+
+
 /**
  * @author Jason Yu
  */
 public class GroovyMarkupViewTests {
+
+	private static final String RESOURCE_LOADER_PATH = "classpath*:org/springframework/web/reactive/result/view/groovy";
+
+	private final MockServerWebExchange exchange = MockServerHttpRequest.get("/path").toExchange();
+
+	private GenericApplicationContext context;
+
+//	private GroovyMarkupConfig groovyMarkupConfig;
+
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
+
+	@Before
+	public void setup() throws Exception {
+		this.context = new GenericApplicationContext();
+		this.context.refresh();
+
+//		GroovyMarkupConfigurer configurer = new GroovyMarkupConfigurer();
+//		configurer.setResourceLoaderPath(RESOURCE_LOADER_PATH);
+//		this.groovyMarkupConfig = configurer;
+	}
+
+
+	@Test
+	public void missingGroovyMarkupConfig() throws Exception {
+		this.exception.expect(ApplicationContextException.class);
+		this.exception.expectMessage("Expected a single GroovyMarkupConfig bean in");
+
+		GroovyMarkupView view = new GroovyMarkupView();
+		view.setApplicationContext(this.context);
+		view.setUrl("sampleView");
+		view.afterPropertiesSet();
+	}
+
+	@Test
+	public void customTemplateEngine() throws Exception {
+		GroovyMarkupView view = new GroovyMarkupView();
+		view.setTemplateEngine(new TestTemplateEngine());
+		view.setApplicationContext(this.context);
+
+		DirectFieldAccessor accessor = new DirectFieldAccessor(view);
+		TemplateEngine engine = (TemplateEngine)accessor.getPropertyValue("engine");
+		assertNotNull(engine);
+		assertEquals(TestTemplateEngine.class, engine.getClass());
+	}
+
+	public class TestTemplateEngine extends MarkupTemplateEngine {
+
+		public TestTemplateEngine() {
+			super(new TemplateConfiguration());
+		}
+
+		@Override
+		public Template createTemplate(Reader reader) {
+			return null;
+		}
+	}
 }

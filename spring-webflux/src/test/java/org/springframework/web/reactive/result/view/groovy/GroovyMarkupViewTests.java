964937e6b9805b69525e00c02d6c054d52860b61
Jason Yu <jason.yu@fuhu.com>
2017-08-15 11:12:37 +0800

    Add render template cases to groovy markup view tests

--- spring-webflux/src/test/java/org/springframework/web/reactive/result/view/groovy/GroovyMarkupViewTests.java
+++ spring-webflux/src/test/java/org/springframework/web/reactive/result/view/groovy/GroovyMarkupViewTests.java
@@ -17,12 +17,18 @@
 package org.springframework.web.reactive.result.view.groovy;
 
 import java.io.Reader;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import groovy.text.Template;
 import groovy.text.TemplateEngine;
 import groovy.text.markup.MarkupTemplateEngine;
 import groovy.text.markup.TemplateConfiguration;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -34,8 +40,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.mock.http.server.reactive.test.MockServerHttpRequest;
 import org.springframework.mock.http.server.reactive.test.MockServerWebExchange;
+import reactor.test.StepVerifier;
 
 import static org.junit.Assert.*;
 
@@ -47,11 +55,7 @@ public class GroovyMarkupViewTests {
 
 	private static final String RESOURCE_LOADER_PATH = "classpath*:org/springframework/web/reactive/result/view/groovy/";
 
-	private final MockServerWebExchange exchange = MockServerHttpRequest.get("/path").toExchange();
-
 	private GenericApplicationContext context;
-
-//	private GroovyMarkupConfig groovyMarkupConfig;
 
 	@Rule
 	public final ExpectedException exception = ExpectedException.none();
@@ -60,10 +64,6 @@ public class GroovyMarkupViewTests {
 	public void setup() throws Exception {
 		this.context = new GenericApplicationContext();
 		this.context.refresh();
-
-//		GroovyMarkupConfigurer configurer = new GroovyMarkupConfigurer();
-//		configurer.setResourceLoaderPath(RESOURCE_LOADER_PATH);
-//		this.groovyMarkupConfig = configurer;
 	}
 
 
@@ -114,6 +114,52 @@ public class GroovyMarkupViewTests {
 		assertTrue(view.checkResourceExists(Locale.CHINESE));
 	}
 
+	@Test
+	public void renderMarkupTemplate() throws Exception {
+		Map<String, Object> model = new HashMap<>();
+		model.put("name", "Spring");
+		MockServerWebExchange exchange = renderViewWithModel("test.tpl", model, Locale.US);
+		StepVerifier.create(exchange.getResponse().getBody())
+				.consumeNextWith( buf -> assertThat(asString(buf), Matchers.containsString("<h1>Hello Spring</h1>")))
+				.expectComplete()
+				.verify();
+	}
+
+	@Test
+	public void renderI18nTemplate() throws Exception {
+		Map<String, Object> model = new HashMap<>();
+		model.put("name", "Spring");
+		MockServerWebExchange exchange = renderViewWithModel("i18n.tpl", model, Locale.FRANCE);
+		StepVerifier.create(exchange.getResponse().getBody())
+				.consumeNextWith( buf -> assertEquals("<p>Bonjour Spring</p>", asString(buf)))
+				.expectComplete()
+				.verify();
+
+		exchange = renderViewWithModel("i18n.tpl", model, Locale.GERMANY);
+		StepVerifier.create(exchange.getResponse().getBody())
+				.consumeNextWith( buf -> assertEquals("<p>Include German</p><p>Hallo Spring</p>", asString(buf)))
+				.expectComplete()
+				.verify();
+
+		exchange = renderViewWithModel("i18n.tpl", model, new Locale("es"));
+		StepVerifier.create(exchange.getResponse().getBody())
+				.consumeNextWith( buf -> assertEquals("<p>Include Default</p><p>Hola Spring</p>", asString(buf)))
+				.expectComplete()
+				.verify();
+	}
+
+	@Test
+	public void renderLayoutTemplate() throws Exception {
+		Map<String, Object> model = new HashMap<>();
+		MockServerWebExchange exchange = renderViewWithModel("content.tpl", model, Locale.US);
+		StepVerifier.create(exchange.getResponse().getBody())
+				.consumeNextWith( buf -> assertEquals(
+						"<html><head><title>Layout example</title></head><body><p>This is the body</p></body></html>",
+						asString(buf)))
+				.expectComplete()
+				.verify();
+	}
+
 	private GroovyMarkupView createViewWithUrl(String viewUrl) throws Exception {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 		ctx.register(GroovyMarkupConfiguration.class);
@@ -124,6 +170,22 @@ public class GroovyMarkupViewTests {
 		view.setApplicationContext(ctx);
 		view.afterPropertiesSet();
 		return view;
+	}
+
+	private MockServerWebExchange renderViewWithModel(
+			String viewUrl, Map<String, Object> model, Locale locale) throws Exception {
+		GroovyMarkupView view = createViewWithUrl(viewUrl);
+		MockServerWebExchange exchange = MockServerHttpRequest.get("/path")
+				.acceptLanguageAsLocales(locale).toExchange();
+		view.render(model, null, exchange).block(Duration.ofSeconds(5));
+		return exchange;
+	}
+
+	private static String asString(DataBuffer dataBuffer) {
+		ByteBuffer byteBuffer = dataBuffer.asByteBuffer();
+		final byte[] bytes = new byte[byteBuffer.remaining()];
+		byteBuffer.get(bytes);
+		return new String(bytes, StandardCharsets.UTF_8);
 	}
 
 	public class TestTemplateEngine extends MarkupTemplateEngine {
